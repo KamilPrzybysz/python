77a8bd47fd48a195f92684f79ea0cb8a1b6e2128
Kamil Przybysz <kprzybysz@sigma-ng.inf.ug.edu.pl>
2018-03-27 13:50:00 +0200

    asdfas

---
+++ 25.03.18/PRIME_T-Liczby_pierwsze.py
@@ -0,0 +1,17 @@
+def funkcja(aa):
+        liczby=[]
+        for i in range(aa):
+            liczby.append(int(input()))
+        for i in range(aa):
+            if(int(liczby[i])>10000 or liczby[i]>=2):
+                for x in range(2, liczby[i]):
+                    if(liczby[i]%x==0):
+                        print("NIE")
+                        break
+                else:
+                     print("TAK")
+            else:
+                print("NIE")
+        return exit() a=int(input()) if(a>=100000):
+        exit()
+print(funkcja(a))
--- 25.03.18/VSR-Predkosc_srednia.py
+++ 25.03.18/VSR-Predkosc_srednia.py
@@ -1,13 +1,17 @@
-def funkcja(aa):
-	for i in range(aa):
-		wynik=[]
+def funkcja(tt):
+	wynik=[]
+	for i in range(tt):
 		liczby=input()
 		liczby=liczby.split()
+		if(int(liczby[0])<=1 or int(liczby[1])>=10000):
+			return exit()
 		wynik.append((2*(int(liczby[0])*(int(liczby[1]))))/(int(liczby[0])+(int(liczby[1]))))
-		print(wynik[i])
-
+	for i in range(tt):
+		print(int(wynik[i]))
 	return exit()
 
 
-a=int(input())
-print(funkcja(a))
+t=int(input())
+if(t<=1 or t>=10000):
+	exit()
+print(funkcja(t))
