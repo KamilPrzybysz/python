f03d1d25725d1783a53183f5c255b55df95d7573
Kamil Przybysz <kprzybysz@sigma-ng.inf.ug.edu.pl>
2018-03-27 16:18:52 +0200

    asdsad

--- 25.03.18/FCTRL3-Dwie_cyfry_silni.py
+++ 25.03.18/FCTRL3-Dwie_cyfry_silni.py
@@ -1,35 +1,27 @@
 def funkcja(dd):
-	n=[]
-	for i in range(d-1):
-		n.append(int(input()))
-		if(n[i]<0 or n[i]>1000000000):
-        		exit()
-
-	else:
-		for j in range(d-1):
-			wynik=1
-			dziesiec=0
-			jednosc=0
-			if(n[j]<=1):
-				wynik=1
-				dziesiec=0
-				jednosc=1
-				print(str(dziesiec))
-				print((jednosc))
-			else:
-				for x in range(n[j]):
-					wynik=str(n[j]*wynik)
-				dziesiec=wynik[len(wynik)-2]
-				jednosc=wynik[len(wynik)-2]
-				return dziesiec
-				return jednosc
-
+        wynik=[]
+        for i in range(dd):
+                silnia=1
+                n=int(input())
+                if(n<0 or n>1000000000):
+                        exit()
+                elif(n>=10):
+                    wynik.append(str(0))
+                    wynik.append(str(" "))
+                    wynik.append(str(0))
+                    wynik.append(str('\n'))
+                else:
+                    for j in range(1,(n+1)):
+                            silnia=int(silnia)*int(j)
+                    dziesiec=int((silnia%100)/10)
+                    jednosc=int(silnia%10)
+                    wynik.append(str(dziesiec))
+                    wynik.append(str(" "))
+                    wynik.append(str(jednosc))
+                    wynik.append(str('\n'))
+        return ''.join(wynik)
 
 d=int(input())
 if(d<1 or d>30):
-	exit()
-n=int(input())
-if(n<0 or n>1000000000):
-	exit()
-
+        exit()
 print(funkcja(d))
